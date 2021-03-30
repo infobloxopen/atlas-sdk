788aec15583d495b95f3582d1cc080453f50d045
Drew Wells <drew.wells00@gmail.com>
2021-03-30 11:05:37 -0500

    remove local proto

--- proto/tools.go
+++
@@ -1,8 +0,0 @@
-//+build tools
-package tools
-
-import (
-	_ "github.com/infobloxopen/atlas-app-toolkit/query"
-	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
-)
